d30868e3ee32ce75d383c69317dc235b80a309f9
MinhChieens <114223606+MinhChieens@users.noreply.github.com>
2024-04-02 22:01:07 +0700

    reorganize the project structure and CSS headinfo

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
 import "./index.css";
-import Navbar from "./components/Navbar";
-
+import { Route, Routes } from "react-router-dom";
+import About from "./pages/About";
+import Doctors from "./pages/Doctors";
+import Home from "./pages/Home";
+import HomeInfo from "./pages/HomeInfo";
+import Login from "./pages/Login";
+import Services from "./pages/Services";
+import SignUp from "./pages/SignUp";
 const App = () => {
-  return (
-    <>
-      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-        <Navbar />
-      </div>
-    </>
-  );
+   return (
+      <>
+         <Routes>
+            <Route path="/" element={<HomeInfo></HomeInfo>} />
+            <Route path="/home" element={<Home></Home>} />
+            <Route path="/services" element={<Services></Services>} />
+            <Route path="/about" element={<About></About>} />
+            <Route path="/doctors" element={<Home></Home>} />
+            <Route path="/news" element={<Home></Home>} />
+            <Route path="/login" element={<Home></Home>} />
+            <Route path="/signUp" element={<Home></Home>} />
+         </Routes>
+      </>
+   );
 };
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,22 +2,23 @@
 import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      colors: {
-        darkblue: "#1F2B6C",
-        skyblue: "#159EEC",
-        lightblue: "#BFD2F8",
-        white: "#FFFFFF",
-        black: "#000000",
+   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+   theme: {
+      extend: {
+         colors: {
+            darkblue: "#1F2B6C",
+            skyblue: "#159EEC",
+            lightblue: "#BFD2F8",
+            white: "#FFFFFF",
+            black: "#000000",
+         },
+         fontFamily: {
+            sans: ["var(--font-sora)", ...fontFamily.sans],
+            code: "var(--font-code)",
+            grotesk: "var(--font-grotesk)",
+            worksans: ['"Work Sans"', "sans-serif"],
+         },
       },
-      fontFamily: {
-        sans: ["var(--font-sora)", ...fontFamily.sans],
-        code: "var(--font-code)",
-        grotesk: "var(--font-grotesk)",
-      },
-    },
-  },
-  plugins: [],
+   },
+   plugins: [],
 };
