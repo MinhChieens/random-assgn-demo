2b01837bbce4487df5998e9ca36b24c2cde53a0f
kng1259 <khoinguyenanlac1@gmail.com>
2024-04-02 16:48:49 +0700

    modified:   index.html 	modified:   src/components/Header.jsx 	modified:   src/constants/index.js 	modified:   src/index.css 	modified:   tailwind.config.js

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,38 +6,33 @@ const Header = () => {
   const pathname = useLocation();
 
   return (
-    <div
-      className={`fixed top-0 left-0 w-full z-50  border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm   bg-n-8" `}
-    >
-      <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
-        <a className="block w-[12rem] xl:mr-8" href="#hero">
-          <img src="./vite.svg" alt="Hospital" />
+    <div className={`fixed top-0 left-0 w-full flex items-center bg-darkblue`}>
+      <nav className="static flex mx-auto">
+        {navigation.map((item) => (
+          <a
+            key={item.id}
+            href={item.url}
+            className={`block relative font-sans text-white/50 transition-colors hover:text-white px-4 py-6 text-base leading-5 ${
+              item.url === pathname.hash
+                ? "z-2 lg:text-white"
+                : "lg:text-white/50"
+            }`}
+          >
+            {item.title}
+          </a>
+        ))}
+      </nav>
+      <nav className="static flex m-auto">
+        <a className="" href="#search">
+          <img src="./vite.svg" alt="search" />
         </a>
-
-        <nav className="flex fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent">
-          <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
-            {navigation.map((item) => (
-              <a
-                key={item.id}
-                href={item.url}
-                className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 $ px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold lg:leading-5 lg:hover:text-n-1 xl:px-12 ${
-                  item.url === pathname.hash
-                    ? "z-2 lg:text-n-1"
-                    : "lg:text-n-1/50"
-                }`}
-              >
-                {item.title}
-              </a>
-            ))}
-          </div>
-        </nav>
         <a
-          className="mr-8 font-code uppercase text-n-1/50 transition-colors hover:text-n-1 lg:block"
-          href="#login"
+          className="mx-4 font-sans text-sm font-bold text-darkblue/80 transition-colors hover:text-darkblue block px-6 py-3 bg-solid rounded-full bg-lightblue"
+          href="#appointment"
         >
-          Log in
+          Appointment
         </a>
-      </div>
+      </nav>
     </div>
   );
 };
