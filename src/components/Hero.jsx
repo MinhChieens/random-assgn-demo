22c35c8c695483bcb5c68098ca361e315f4f94d9
kng1259 <khoinguyenanlac1@gmail.com>
2024-04-05 22:15:17 +0700

    new file:   src/assets/bg-doctor.png 	modified:   src/components/Footer.jsx modified:   src/components/Hero.jsx 	modified:   src/index.css

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,19 +2,21 @@ import Button from "../components/Button";
 
 const Hero = () => {
   return (
-    <section className="relative top-0 left-0 w-full bg-[url('../src/assets/hero-img.png')] bg-no-repeat bg-cover flex-col align-center justify-start">
-      <h2 className="text-skyblue uppercase font-bold font-worksans">
-        Caring for life
-      </h2>
-      <h1 className="text-darkblue font-bold font-worksans text-5xl">
-        Leading the Way <br /> in Medical Excellence
-      </h1>
-      <Button
-        className="bg-lightblue rounded-full font-sm px-4 py-2"
-        href="services"
-      >
-        Our Services
-      </Button>
+    <section className="relative min-w-full aspect-[1125/512] bg-[url('../src/assets/bg-doctor.png')] bg-no-repeat bg-cover flex flex-col justify-center">
+      <div className="px-36">
+        <h3 className="text-skyblue uppercase font-bold font-worksans my-2">
+          Caring for life
+        </h3>
+        <h1 className="text-darkblue font-bold font-worksans text-5xl my-2">
+          Leading the Way <br /> in Medical Excellence
+        </h1>
+        <Button
+          className="bg-lightblue rounded-full font-bold font-sm px-4 py-2 my-2"
+          href="services"
+        >
+          Our Services
+        </Button>
+      </div>
     </section>
   );
 };
