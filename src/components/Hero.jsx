c0f626c0f1f5afa89e6871c89f7b16b74fc9c52e
kng1259 <khoinguyenanlac1@gmail.com>
2024-04-06 15:43:33 +0700

    improve 3 appointment buttons

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,11 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Button from "../components/Button";
+
+import {
+   faCalendar,
+   faUser,
+   faMoneyBill1,
+} from "@fortawesome/free-regular-svg-icons";
 
 const Hero = () => {
    return (
@@ -22,15 +29,27 @@ const Hero = () => {
 
          <div className="welcome relative flex flex-col justify-center items-center">
             <div className="appointment relative top-[-3rem] flex flex-row justify-center items-center gap-12">
-               <div className="book w-[20rem] rounded h-[6rem] bg-[#1F2B6C] flex items-center justify-center text-white">
+               <Button
+                  href="appointment"
+                  className="book w-[20rem] rounded h-[6rem] bg-[#1F2B6C] flex items-center justify-around text-white/80 hover:text-white"
+               >
                   <p>Book an Appointment</p>
-               </div>
-               <div className="book w-[20rem] rounded h-[6rem] bg-[#BFD2F8] flex items-center justify-center text-darkblue">
+                  <FontAwesomeIcon icon={faCalendar} size="2x" />
+               </Button>
+               <Button
+                  href="appointment"
+                  className="book w-[20rem] rounded h-[6rem] bg-[#BFD2F8] flex items-center justify-around text-darkblue/80 hover:text-darkblue"
+               >
                   <p>Book an Appointment</p>
-               </div>
-               <div className="book w-[20rem] rounded h-[6rem] bg-[#159EEC] flex items-center justify-center text-white">
+                  <FontAwesomeIcon icon={faUser} size="2x" />
+               </Button>
+               <Button
+                  href="appointment"
+                  className="book w-[20rem] rounded h-[6rem] bg-[#159EEC] flex items-center justify-around text-white/80 hover:text-white"
+               >
                   <p>Book an Appointment</p>
-               </div>
+                  <FontAwesomeIcon icon={faMoneyBill1} size="2x" />
+               </Button>
             </div>
             <div className="content w-[40rem] flex flex-col justify-center items-center ">
                <h4 className="  font-bold text-lg text-center text-sky-500 uppercase tracking-[2.88px]">
