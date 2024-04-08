e54d2a08d3b8d8e96e480433113554771c0133fa
kng1259 <khoinguyenanlac1@gmail.com>
2024-04-08 23:06:31 +0700

    add Appointment component's styling

--- src/pages/HomeInfo.jsx
+++ src/pages/HomeInfo.jsx
@@ -3,12 +3,14 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Hero from "../components/Hero";
 import Contact from "../components/Contact";
+import Appointment from "../components/Appointment";
 
 const HomeInfo = () => {
    return (
       <>
          <Navbar></Navbar>
          <Hero></Hero>
+         <Appointment></Appointment>
          <Contact></Contact>
          <Footer></Footer>
       </>
