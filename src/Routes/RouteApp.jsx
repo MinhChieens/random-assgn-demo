6c36a63eae36d1ed02304edc8317e94ac95c7044
MinhChieens <114223606+MinhChieens@users.noreply.github.com>
2024-04-28 08:50:40 +0700

    Dashboard doctors init

--- src/Routes/RouteApp.jsx
+++ src/Routes/RouteApp.jsx
@@ -2,7 +2,7 @@ import Dashboard from "../pages/pagesPatient/Dashboard";
 import Support from "../pages/pagesPatient/Support";
 import DashboardAdmin from "../pages/pagesAdmin/DashboardAdmin";
 import SupportAdmin from "../pages/pagesAdmin/SupportAdmin";
-import DashboardDoctor from "../pages/pagesPatient/Dashboard";
+import DashboardDoctor from "../pages/pagesDoctors/DashboardDoctor";
 import SupportDoctor from "../pages/pagesDoctors/SupportDoctor";
 import ListDoctors from "../pages/pagesAdmin/ListDoctors";
 import DoctorScheduling from "../pages/pagesDoctors/DoctorScheduling";
@@ -21,7 +21,7 @@ export const RouteUser = [
   { path: "information", element: <Infomation />, sidebarProps: {} },
 ];
 export const RouteDoctor = [
-  { path: "home", element: <DashboardDoctor />, state: "home" },
+  { path: "", element: <DashboardDoctor />, state: "home" },
   { path: "dashboard", element: <DashboardDoctor />, sidebarProps: {} },
   { path: "support", element: <SupportDoctor />, sidebarProps: {} },
   { path: "schedule", element: <DoctorScheduling />, sidebarProps: {} },
