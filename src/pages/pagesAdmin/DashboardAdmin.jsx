f9cb9ea1a3fd94e76c6cd6e8de0be45b63645067
duydngn89 <duy.nguyend8998@hcmut.edu.vn>
2024-05-19 12:43:23 +0700

    update

--- src/pages/pagesAdmin/DashboardAdmin.jsx
+++ src/pages/pagesAdmin/DashboardAdmin.jsx
@@ -1,9 +1,32 @@
 import React from "react";
 import { homeAdmin } from "../../constants/dashboardHome";
-import DashboardHome from "../../components/Dashboard/DashboardHome";
+import Leaderboard from "../../components/Dashboard/DashboardHome";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+   faUserDoctor,
+   faUser,
+   faStaffAesculapius,
+   faMicroscope,
+   faSyringe
+ } from "@fortawesome/free-solid-svg-icons";
 
 const DashboardAdmin = () => {
-   return <DashboardHome title="Dashboard Admin" homeContent={homeAdmin} />;
+   return (
+   <div className="App flex flex-col justify-around my-4  gap-4">
+      <div className="flex flex-row justify-around my-4  gap-4">
+        <Leaderboard name="Doctor" number="Number" icon={faUserDoctor}  />
+        <Leaderboard name="Patient" number="Top Scorer" icon={faUser}  />
+        <Leaderboard name="Staff" number="Highest Points" icon={faStaffAesculapius} />
+        
+      </div>
+      <div className="flex flex-row justify-around my-4  gap-4">
+        <Leaderboard name="Medical Devices" number="Leaderboard" icon={faMicroscope}  />
+        <Leaderboard name="Medicines" number="Number" icon={faSyringe}  />
+        <Leaderboard name="Apponinment" number="Number" icon={faSyringe} />
+        
+      </div>
+    </div>
+   );
 };
 
 export default DashboardAdmin;
